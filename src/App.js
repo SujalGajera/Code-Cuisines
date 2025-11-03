6c3c3bf8adb34b7d69cc84410b76bef2fd7708ce
roshan8907 <roshandhakal788@gmail.com>
2025-11-04 10:54:21 +1300

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Sujal/Navbar';
-import Home from './Sujal/Home';
- 
-// Placeholder components
-const Explore = () => <div>Explore Page</div>;
-const About = () => <div>About Us Page</div>;
- 
-const AdminLogin = () => <div>Admin Login Page</div>;
-const StaffLogin = () => <div>Staff/Receptionist Login Page</div>;
-const CustomerLogin = () => <div>Customer Login Page</div>;
- 
+import React from "react";
+import "./App.css";
+import SignUp from "./SignUp";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login/admin" element={<AdminLogin />} />
-        <Route path="/login/staff" element={<StaffLogin />} />
-        <Route path="/login/customer" element={<CustomerLogin />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <SignUp />
+    </div>
   );
 }
- 
+
 export default App;
- 
